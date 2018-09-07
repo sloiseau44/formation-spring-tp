ae01a397addab40d852bc9ce41bf81598d4c70d2
Guillaume EHRET Dev Mind <guillaume@dev-mind.fr>
2018-09-07 14:30:21 +0200

    TP03 Démarrer avec Spring et Java config :-)

--- src/main/java/com/training/springcore/BigCorpApplication.java
+++ src/main/java/com/training/springcore/BigCorpApplication.java
@@ -2,18 +2,18 @@ package com.training.springcore;
 
 import com.training.springcore.service.SiteService;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class BigCorpApplication {
 
-    public static void main (String[] args){
+    public static void main(String[] args) {
         BigCorpApplication application = new BigCorpApplication();
         application.run();
     }
 
-    public void run(){
+    public void run() {
         System.out.println("Application startup");
-        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(BigCorpApplicationConfig.class);
         SiteService siteService = context.getBean(SiteService.class);
         System.out.println(siteService.findById("siteA"));
         SiteService siteService2 = context.getBean(SiteService.class);
