4473a17372b616534e23cdc1b8cb0488572e90f5
Guillaume EHRET Dev Mind <guillaume@dev-mind.fr>
2018-09-07 10:19:15 +0200

    TP0 adds entities to be able to start the training

---
+++ src/main/java/com/training/springcore/BigCorpApplication.java
@@ -0,0 +1,13 @@
+package com.training.springcore;
+
+public class BigCorpApplication {
+
+    public static void main (String[] args){
+        BigCorpApplication application = new BigCorpApplication();
+        application.run();
+    }
+
+    public void run(){
+        System.out.println("Application startup");
+    }
+}
---
+++ src/main/java/com/training/springcore/service/SiteServiceImpl.java
@@ -0,0 +1,20 @@
+package com.training.springcore.service;
+
+import com.training.springcore.model.Site;
+
+public class SiteServiceImpl implements SiteService {
+
+    private CaptorService captorService = new CaptorServiceImpl();
+
+    @Override
+    public Site findById(String siteId) {
+        if (siteId == null) {
+            return null;
+        }
+
+        Site site = new Site("Florange");
+        site.setId(siteId);
+        site.setCaptors(captorService.findBySite(siteId));
+        return site;
+    }
+}
